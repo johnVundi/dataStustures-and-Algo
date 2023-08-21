f027f9a24e4bbffe0b5d2ac5aa396623064a64e1
John Vundi <johnsonvundy2@gmail.com>
2023-08-21 07:53:22 +0300

    begin practice

--- commonArray.js
+++ commonArray.js
@@ -1,17 +1,39 @@
+// const array1 = ['a','d', 'e', 'k', 'o', 'p']
+// const array2 = ['b', 'n', 'm', 'j', 'u']
+
+// function commonArray(array1, array2){
+//     for( let i = 0; i < array1.length; i++){
+//         for(let j = 0; j < array2.length; j++){
+//             if(array1[i] === array2[j]){
+//                 return true;
+//             }
+//         }
+
+//     }
+
+//     return false;
+// }
+
+// console.log(commonArray(array1,array2))
+
 const array1 = ['a','d', 'e', 'k', 'o', 'p']
-const array2 = ['b', 'n', 'm', 'j', 'f',]
+const array2 = ['b', 'n', 'm', 'j', 'e']
 
-function commonArray(aray){
-    for( let i = 0; i < array1.length; i++){
-        for(let j = 0; j < array2.length; j++){
-            if(array1[aray[i]] === array2[aray[j]]){
-                return true;
-            }
+function commonArray(array1, array2){
+    let map = {}
+
+    for(let i = 0; i < array1.length; i++){
+        if(!map[array1[i]]){
+            const item = array1[i]
+            map [item]= true;
         }
-
     }
-
-    return false;
+    for(let j = 0; j < array2.length; j++){
+        if(map[array2[j]]){
+            return true
+        }
+    }
+return false
 }
 
 console.log(commonArray(array1,array2))
